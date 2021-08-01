2adc6555d916460af6d78ba88d675446c6085ebf
Felix von Tiedemann <vontifel@students.zhaw.ch>
2021-08-01 15:12:41 +0200

    Add basic js error display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -41,6 +41,12 @@ document.addEventListener("turbolinks:load", function() {
 
     Credential.get(credentialOptions);
   })
+  $(document).on('ajax:error', function(xhr, status, error) {
+    let alert = document.createElement('div');
+    alert.className = 'alert alert-dissmissible mt-3 alert-danger';
+    alert.innerText = xhr.detail[0];
+    $('#flashbox').append(alert);
+  });
 })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
